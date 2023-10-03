57039f59dccf966dcbd0b2535064039ad1076868
Lucena-Lucas31 <lucaslucena31@hotmail.com>
2023-10-02 22:15:30 -0300

    Atividade de Rotas com React

---
+++ Exercício4_Rotas_Lucas_Sampaio_Leticia/app/cena2.js
@@ -0,0 +1,153 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, } from 'react-native';
+import { Link } from 'expo-router';
+import { ImageBackground, Image, TextInput, Button, Alert, Center, Pressable } from "react-native";
+
+
+function Btn() {
+  
+  return <Image source={require('./assets/home.png')} alt="play" w="50%" style={styles.play}/>
+}
+
+export default function App() {
+
+  const [text1, onChangeText1] = React.useState('');
+  const [text2, onChangeText2] = React.useState('');
+
+  const [number, onChangeNumber] = React.useState('');
+
+  return (
+
+  <View style={styles.container}>
+
+    <ImageBackground source={require('./assets/Parabens.png')}
+     style={styles.imageBackground}>
+
+    
+    <Text style={styles.emailText} >
+
+
+    </Text>
+
+
+    <Text style={styles.senhaText}>
+
+
+
+    </Text>
+
+    
+
+        <Text>
+             {"\n"}
+             
+        </Text>
+
+    
+    
+    <Link href="/cena0" style={styles.play}>
+    <Btn></Btn>   
+    </Link>
+
+    
+
+    </ImageBackground>
+
+
+
+  </View>
+
+
+)}
+
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    flexDirection: "column"
+  },
+
+  emailText: {
+    fontSize: 60,
+    color: '#000000',
+    marginBottom: 10,
+    marginLeft: 50,
+
+    position: 'absolute',
+    top: 50,
+    left: 40,
+
+    shadowColor: '#171717',
+    shadowOffset: {width: -2, height: 4},
+    shadowOpacity: 0.4,
+    shadowRadius: 3,
+  },
+
+  button: {
+    
+    left: 90,
+    height: 40,
+    padding: 1,
+    borderRadius: 20,
+    top: 400,
+  },
+  play: {
+    height: 80,
+    width: 80,  
+
+    top: 300,
+    left: 80,
+  },
+
+
+  envio: {
+    color: '#FFFFFF',
+    fontSize: 40,
+    fontFamily: 'Roboto',
+    
+
+  },
+  
+  senhaText: {
+    fontSize: 19,
+    color: '#8E0033',
+    marginTop: 18,
+    marginBottom: 10,
+    marginLeft: 50,
+
+    shadowColor: '#171717',
+    shadowOffset: {width: -2, height: 4},
+    shadowOpacity: 0.4,
+    shadowRadius: 3,
+  },
+  
+  input: {
+    marginLeft: 30,
+    marginRight: 30,
+    height: 60,
+    borderWidth: 2,
+    padding: 10,
+    borderColor: '#FF005C',
+    backgroundColor: 'rgba(241, 184, 205, 0.5)',
+    borderRadius: 20,
+
+    fontSize: 20,
+
+    shadowColor: '#171717',
+    shadowOffset: {width: -2, height: 4},
+    shadowOpacity: 0.3,
+    shadowRadius: 3,
+  },
+  imageBackground: {
+    flex: 1,
+    resizeMode: "cover",
+  },
+  imageSW: {
+    resizeMode: "cover",
+    width: 360,
+    position: 'absolute',
+    top: 30,
+    left: 32,
+  },
+  
+});
